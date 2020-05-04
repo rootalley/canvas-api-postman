004c448aaeddae264a22d7f3ceec3b7d290efc68
Damian Sweeney <dsweeney@unimelb.edu.au>
2020-05-05 09:47:27 +1000

    Added notes on Postman set up and more documentation to the main script

--- README.md
+++ README.md
@@ -7,3 +7,10 @@ Script to extract the current API documentation from Canvas and generate an impo
 Outputs an 'output.json' file that is used for import into Postman
 
 In Postman choose Import -> Import File -> navigate to this folder
+
+Once in Postman you'll need to set ```domian``` and ```user_token``` variables.
+
+For example:
+
+```domain: yourschool.instructure.com```
+```user_token: 12345~abcdefg...789```
--- canvas-api-postman.py
+++ canvas-api-postman.py
@@ -8,8 +8,10 @@ postman_domain = '{{domain}}'
 print('Getting base doc')
 top_level_docs = requests.get('{}/doc/api/api-docs.json'.format(base_url))
 
+# get the API portion of the JSON object returned from Canvas
 docs = top_level_docs.json()['apis']
 
+# create a framework JSON object to collect the different APIs
 postman_collection = {
 	'info': {
 		'name': 'Canvas API',
@@ -28,15 +30,22 @@ postman_collection = {
 	}
 }
 
+# loop through each of the APIs
+# see here for the full list: https://canvas.instructure.com/doc/api/api-docs.json
 for doc in docs:
 	folder = {
 		'name': doc['description'],
 		'item': []
 	}
+
+	# get the JSON object for this API endpoint
 	print('Getting {} doc'.format(folder['name']))
 	raw_doc_request = requests.get('{}/doc/api{}'.format(base_url,doc['path']))
 	doc_detail = raw_doc_request.json()
+
+	# loop though the different calls for this API
 	for item in doc_detail['apis']:
+		# construct the path (including path variables)
 		path_array = [
 			'api'
 			]
@@ -44,6 +53,7 @@ for doc in docs:
 		path_elements = re.findall('(\/[^/]*)', item['path'])
 		for el in path_elements:
 			el = el[1:]
+			# path variables are translated here from {course_id} to :course_id
 			if '{' in el:
 				path_array_items = re.sub('{',':',re.sub('}','',el))
 				path_array.append(path_array_items)
@@ -51,7 +61,9 @@ for doc in docs:
 				path_array.append(el)
 		revised_path = '/'.join(path_array)
 
+		# form parameters appear in the 'body' section in Postman
 		form_parameters = []
+		# variables appear as path variables in Postman in the Params section
 		variable_array = []
 		if 'parameters' in list(item['operations'][0]):
 			for param in item['operations'][0]['parameters']:
@@ -100,5 +112,6 @@ for doc in docs:
 		folder['item'].append(api_call)
 	postman_collection['item'].append(folder)
 
+# write the collection to a file
 with open('output.json', 'w') as f:
 	json.dump(postman_collection, f, indent=3, separators=(',', ': '))
